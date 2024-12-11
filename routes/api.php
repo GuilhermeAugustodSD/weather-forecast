32c864f3b2d7dd29bfd7019bd716ea0d908466a0
Guilherme Dantas <guilhermednts2@gmail.com>
2024-12-11 18:23:16 -0300

    feat/ habilitando rota api no projeto

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WeatherController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::controller(WeatherController::class)->prefix("/weather")->group(function(){
+  Route::get('/complete/{search}', 'getAutoCompleteLocation')->name('weather.getAutoCompleteLocation');
+  Route::get('/{search}', 'getWeather')->name('weather.getWeather');
+});
