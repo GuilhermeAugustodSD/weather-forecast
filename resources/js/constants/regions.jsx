cfef4d9a924cb4aef028655203acc2b6af398472
Guilherme Dantas <guilhermednts2@gmail.com>
2024-12-12 14:34:41 -0300

    feat/ contantes dos estados

---
+++ resources/js/constants/regions.jsx
@@ -0,0 +1,31 @@
+const regions = {
+  'Acre': 'AC',
+  'Alagoas': 'AL',
+  'Amapa': 'AP',
+  'Amazonas': 'AM',
+  'Bahia': 'BA',
+  'Ceara': 'CE',
+  'Distrito Federal': 'DF',
+  'Espirito Santo': 'ES',
+  'Goias': 'GO',
+  'Maranhao': 'MA',
+  'Mato Grosso': 'MT',
+  'Mato Grosso do Sul': 'MS',
+  'Minas Gerais': 'MG',
+  'Para': 'PA',
+  'Paraíba': 'PB',
+  'Parana': 'PR',
+  'Pernambuco': 'PE',
+  'Piaui': 'PI',
+  'Rio de Janeiro': 'RJ',
+  'Rio Grande do Norte': 'RN',
+  'Rio Grande do Sul': 'RS',
+  'Rondonia': 'RO',
+  'Roraima': 'RR',
+  'Santa Catarina': 'SC',
+  'Sao Paulo': 'SP',
+  'Sergipe': 'SE',
+  'Tocantins': 'TO'
+};
+
+export { regions }
