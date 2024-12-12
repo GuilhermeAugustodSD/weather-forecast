188f4422e2902ca0ac728ab8bc3a6a43346fb723
Guilherme Dantas <guilhermednts2@gmail.com>
2024-12-12 14:35:59 -0300

    feat/ instalação e aplicação do skeleton

--- resources/js/Components/MainBox/index.jsx
+++ resources/js/Components/MainBox/index.jsx
@@ -2,44 +2,73 @@ import styles from './index.module.scss';
 import Pin from '@/assets/image-4.png';
 import BoxConditions from '../BoxConditions';
 import { useState, useEffect } from 'react';
-import { router } from '@inertiajs/react';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+import { regions } from '@/constants/regions';
 
-export default function MainBox({search}) {
-
-  const [data, setData] = useState({})
+export default function MainBox({ search }) {
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(true);
 
   const handleSave = async () => {
-
+    setLoading(true)
     fetch(route(`weather.getWeather`, { search: search }), {
       method: 'GET',
     })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        setData(data)
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
       })
-      .catch(error => console.error('Error:', error));
+      .catch((error) => console.error('Error:', error));
+  };
+
+  const changeToAcronym = (region) => {
+    if (regions[region]) return regions[region];
+    return region;
   };
 
   useEffect(() => {
-    handleSave()
-  }, [search])
+    handleSave();
+  }, [search]);
 
   return (
     <div className={styles.boxInfo}>
       <div className={styles.city}>
         <img src={Pin} alt="Ícone de Pin" />
-        {data?.location?.name}, {data?.location?.region}
+        {loading ? (
+          <Skeleton width={100} baseColor="#b995db61"/>
+        ) : (
+          `${data?.location?.name}, ${changeToAcronym(data?.location?.region)}`
+        )}
       </div>
       <div className={styles.temperatura}>
-        <span className={styles.numero}>{data?.current?.temp_c && Math.trunc(data?.current?.temp_c)}</span>
-        <span className={styles.medicao}>°C</span>
+        {loading ? (
+          <Skeleton width={100} height={100} baseColor="#b995db61"/>
+        ) : (
+          <>
+            <span className={styles.numero}>
+              {data?.current?.temp_c && Math.trunc(data?.current?.temp_c)}
+            </span>
+            <span className={styles.medicao}>°C</span>
+          </>
+        )}
       </div>
 
       <div className={styles.containerConditions}>
-        <BoxConditions name="Vento" value={data?.current?.wind_kph} />
-        <BoxConditions name="Umidade" value={data?.current?.humidity} />
-        <BoxConditions name="Chuva" value={data?.current?.cloud} />
+        {loading ? (
+          <>
+            <Skeleton height={60} baseColor="#b995db61"/>
+            <Skeleton height={60} baseColor="#b995db61"/>
+            <Skeleton height={60} baseColor="#b995db61"/>
+          </>
+        ) : (
+          <>
+            <BoxConditions name="Vento" value={data?.current?.wind_kph} />
+            <BoxConditions name="Umidade" value={data?.current?.humidity} />
+            <BoxConditions name="Chuva" value={data?.current?.cloud} />
+          </>
+        )}
       </div>
     </div>
   );
